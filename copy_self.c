924248836a0531460a784a695225c2ad12baa89c
Lu Hui <luhux@foxmail.com>
2021-11-09 09:39:08 +0000

    update copy_self.c

--- copy_self.c
+++ copy_self.c
@@ -18,23 +18,27 @@ int main(int argc, char *argv[]) {
 	long int n;  // 读入的字节
 	int buf[BUFSIZE]; // 用于复制文件的buffer
 	char *self_real_path = realpath(argv[0],NULL); // 当前文件的绝对路径
-	char *to_filename = argv[1]; // 要复制到的路径
-	
-	if ((infd = open(self_real_path,O_RDONLY)) == -1) {
-		// 出错处理
-		perror("open() failed: ");
+	char *to_filename = argv[1]; // 要复制到文件
+
+	if (argc < 2) {
+		fprintf(stderr,"No output file name\n");
 		exit(1);
 	}
 
 	// 打开文件，如果文件不存在则创建文件
 	for (;(outfd = open(to_filename,O_WRONLY | O_CREAT | O_TRUNC, FILE_PERM_MODE)) == -1;) {
 		// 尝试解决问题
-		if ( errno == EACCES ) { // 解决权限造成的问题
-			if (chmod(to_filename,FILE_PERM_MODE) == -1) {
-				perror("chmod() failed: ");
-				exit(1);
-			}
+		if (chmod(to_filename,FILE_PERM_MODE) == -1) {
+			perror("chmod() failed: ");
+			exit(1);
 		}
+
+	}
+
+	if ((infd = open(self_real_path,O_RDONLY)) == -1) {
+		// 出错处理
+		perror("open() failed: ");
+		exit(1);
 	}
 
 	// 开始拷贝
